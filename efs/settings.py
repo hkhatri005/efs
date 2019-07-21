80517c6751a803adacd67bc91dff04105d275872
Hitesh Kumar Khatri <46361248+hkhatri005@users.noreply.github.com>
2019-07-20 20:53:05 -0500

    change in static dir

--- efs/settings.py
+++ efs/settings.py
@@ -156,7 +156,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'efs/static'),
 )
 
 # Simplified static file serving.
